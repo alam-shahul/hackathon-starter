b3c6b18d00256874660fc10efc5d109fdf31e05c
Shahul Alam <alam.shahul@gmail.com>
2017-11-10 22:44:14 -0500

    Added SAMattack.py with helper methods.

---
+++ python/SAMattack.py
@@ -0,0 +1,79 @@
+import battlecode
+import time
+import random
+import numpy as np
+
+#Start a game
+game = battlecode.Game('SAMplayer')
+
+start = time.clock()
+
+#define helper functions here
+def nearest_my_glass_state(state, entity):
+    nearest_statue = None
+    nearest_dist = 10000
+    for other_entity in state.get_entities(entity_type=battlecode.Entity.STATUE, team=state.my_team):
+        if(entity == other_entity):
+            continue
+        dist = entity.location.adjacent_distance_to(other_entity.location)
+        if(dist< nearest_dist):
+            dist = nearest_dist
+            nearest_statue = other_entity
+
+    return nearest_statue
+
+def nearest_enemy_glass_state(state, entity):
+    nearest_statue = None
+    nearest_dist = 10000
+    for other_entity in state.get_entities(entity_type=battlecode.Entity.STATUE, team=state.other_team):
+        if(entity == other_entity):
+            continue
+        dist = entity.location.adjacent_distance_to(other_entity.location)
+        if(dist< nearest_dist):
+            dist = nearest_dist
+            nearest_statue = other_entity
+
+    return nearest_statue
+
+def is_small_map(state):
+    return state.map.height <= 10 or state.map.width <= 10
+
+def get_entity_sector(entity):
+    return(int(entity.location.x/5), int(entity.location.y/5))
+
+"""
+Attack code below
+"""
+   
+for state in game.turns():
+    # Your Code will run within this loop
+#    print(len(list(state.get_entities(team=state.my_team))))
+    
+    for entity in state.get_entities(team=state.my_team):
+
+        
+
+        my_location = entity.location
+        near_entities = entity.entities_within_euclidean_distance(1.9)
+        near_entities = list(filter(lambda x: x.can_be_picked, near_entities))
+
+
+
+        for pickup_entity in near_entities:
+            assert entity.location.is_adjacent(pickup_entity.location)
+            if entity.can_pickup(pickup_entity):
+                entity.queue_pickup(pickup_entity)
+
+        enemy_statue = nearest_enemy_glass_state(state, entity)
+        if(statue != None):
+            direction = entity.location.direction_to(enemy_statue.location)
+            if entity.can_throw(direction):
+                entity.queue_throw(direction)
+
+        for direction in np.random.permutation(battlecode.Direction.directions()):
+            if entity.can_move(direction):
+                entity.queue_move(direction)
+
+end = time.clock()
+print('clock time: '+str(end - start))
+print('per round: '+str((end - start) / 1000))
